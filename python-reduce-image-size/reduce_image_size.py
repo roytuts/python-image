62ebcff68021341bf29bf06fbadc459dd584f036
Soumitra <projwebinfo@gmail.com>
2025-08-22 10:10:51 +0530

    Update reduce_image_size.py

--- python-reduce-image-size/reduce_image_size.py
+++ python-reduce-image-size/reduce_image_size.py
@@ -1,19 +1,28 @@
 import os, sys
 from PIL import Image
- 
+
 orig_img_dir = "original_images/"
 dirs = os.listdir(orig_img_dir)
- 
+
 print('Image Size Reduction Started...')
- 
+
 for img in dirs:
     if os.path.isfile(orig_img_dir + img):
         im = Image.open(orig_img_dir + img)
         img_name = os.path.basename(orig_img_dir + img)
-        width, height = im.size       
-        im = im.resize((width, height), Image.ANTIALIAS)
-        im.save('scaled_images/' + img_name)
-        #im.save('optimized_images/' + img_name, optimize=True, quality=95)
-        im.save('optimized_images/' + img_name, optimize=True, quality=85)
- 
+        width, height = im.size
+        
+        # Resize the image
+        new_width = int(width / 2)
+        new_height = int(height / 2)
+        resized_img = im.resize((new_width, new_height), Image.LANCZOS)
+        resized_img.save('scaled_images/' + img_name)
+        
+        # Compress the image
+        im.save('optimized_images/' + img_name, quality=85, optimize=True)
+        
+        # Convert to WEBP
+        img_name_without_extension = os.path.splitext(os.path.basename(img_name))[0]
+        im.save('webp_images/' + img_name_without_extension + '.webp', format="WEBP")
+
 print('Image Size Reduction Done.')
